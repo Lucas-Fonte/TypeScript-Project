2c65af3173374acf1fa509eee481eaa23ee6a5b8
Lucas Souza da Fonte <fontelucas@yahoo.com.br>
2020-01-12 23:56:47 -0300

    first commit

---
+++ src/pages/Game.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-unresolved */
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+import { Container } from './styles';
+
+const Game: React.FC = () => (
+  <Container>
+    <h1>Teste</h1>
+  </Container>
+
+);
+
+export default Game;
